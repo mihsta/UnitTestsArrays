0b6ba07e52f1bac2fd698d5ca4ec7cfcee2d9361
Mikhail Stashko <2338881+mihsta@users.noreply.github.com>
2020-12-14 14:20:39 +0300

    Добавьте файлы проекта.

---
+++ MainModule/ArrayManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainModule
+{
+    public static class ArrayManager
+    {
+        public static int FindMax(int[] a)
+        {
+            
+            int max = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+            }
+            return max;
+        }
+        public static double GetSrednee(int[] a)
+        {
+        int sum = 0;
+        for (int i = 0; i<a.Length; i++)
+            {
+               sum +=a[i];
+            }
+            return (double)sum / a.Length;
+        }
+    }
+}
---
+++ MainModuleTests/ArrayManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MainModule;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainModule.Tests
+{
+    [TestClass()]
+    public class ArrayManagerTests
+    {
+        //https://www.meziantou.net/mstest-v2-data-tests.htm //Про наборы данных можно подключать объекты или базу данных
+        [DataTestMethod]        
+        [DataRow(new[] { 3, 2, 1, 6 }, 6)]
+        [DataRow(new[] { 1, 200, 33 ,8 ,9 ,60}, 200)]
+        [DataRow(new[] { -1, -2, -3 },-1)]
+        [DataRow(new[] { -11, -232, -33 }, -11)]
+        [DataRow(new[] { 1 }, 1)]
+        [DataRow(new[] { 0 }, 0)]
+        [DataRow(new[] { 0,0,0,0,0,0 }, 0)]
+        public void FindMaxTest01_FindMaxInTestArray(int[] testarr, int expected)
+        {            
+            //act
+            int actual = ArrayManager.FindMax(testarr);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [DataTestMethod]
+        [DataRow(new[] { 3, 2, 1, 6 }, 3)]
+        [DataRow(new[] { 1, 200, 33, 8, 9, 60 }, 51.833)]
+        [DataRow(new[] { -1, -2, -3 }, -2)]
+        [DataRow(new[] { -11, -232, -33, 23 }, -63.25)]
+        [DataRow(new[] { 1 }, 1)]
+        [DataRow(new[] { 0 }, 0)]
+        [DataRow(new[] { 0, 0, 0, 0, 0, 0 }, 0)]
+        public void GetSredneeTest_FindAvgInTestArray(int[] testarr, double expected)
+        {
+            //act
+            double actual = ArrayManager.GetSrednee(testarr);
+            //assert
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+    }
+}
